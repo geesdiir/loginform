b9c3857e1cff5e60b6ee8e02ccc3b5ae2a07e495
geesdiir <geesdiir2099@gmail.com>
2024-07-20 17:18:41 +0300

    login form commit

---
+++ lib/components/my_buttom.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyButtom extends StatelessWidget {
+  final Function()? onTap;
+  const MyButtom({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25.0),
+        decoration: BoxDecoration(
+            color: Colors.black, borderRadius: BorderRadius.circular(10)),
+        child: const Center(
+          child: Text(
+            "Sing In",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:loginform/components/my_buttom.dart';
+import 'package:loginform/components/my_textfield.dart';
+import 'package:loginform/components/square_tile.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sining method
+  void singIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              // text widget
+              Text(
+                "Welcome Back you missed!!",
+                style: TextStyle(color: Colors.grey[700], fontSize: 16),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              // userrname field
+              MyTextField(
+                controller: usernameController,
+                hintText: "User Name",
+                obscureText: false,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // password field
+              MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forgot Password",
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // Sing in Buttom
+              MyButtom(
+                onTap: singIn,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.8,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        "Or continue with",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.8,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // google image
+                  SquareTile(imagePath: 'lib/images/google.png'),
+
+                  // apple image
+                  SquareTile(imagePath: 'lib/images/apple.jpeg')
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
